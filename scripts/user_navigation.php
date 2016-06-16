34dd0b5ce9d93817790cbdaf2f185e6df0bb7f44
Edin Melchor <edin.sof@gmail.com>
2016-06-16 18:17:49 -0500

    menu

--- scripts/user_navigation.php
+++ scripts/user_navigation.php
@@ -10,6 +10,6 @@
 <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
   <div class="navbar-header">
     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-    <a class="navbar-brand" href="index.php">SPT</a> </div>
+    <a class="navbar-brand">SPT</a> </div>
   <div style="color:white;padding: 15px 10px 5px 10px;text-align:center;font-size: 16px;"> Hola, <?php echo $nombre; ?>&nbsp; <?php echo $navlinks; ?> </div>
 </nav>
